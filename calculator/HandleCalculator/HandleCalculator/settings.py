78ecefcd819bd326d1476767b5d220a979bae284
Brandon Simmons <90488225+LichuJiang@users.noreply.github.com>
2023-01-18 00:29:14 -0500

    Update settings.py

--- calculator/HandleCalculator/HandleCalculator/settings.py
+++ calculator/HandleCalculator/HandleCalculator/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-g05ifkp(1(fu5t_%sffpf*((a8&)=%5+f!5@zr9(^-%$^#7m=t'
+SECRET_KEY = 'YOUR SECRET KEY'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
